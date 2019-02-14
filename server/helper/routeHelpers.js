836e2b76847850e0237e064d156c4307e55b8cfd
sahilq <iamsahilqureshi@gmail.com>
2019-02-14 12:27:03 +0530

    added joi objectid

--- server/helper/routeHelpers.js
+++ server/helper/routeHelpers.js
@@ -1,13 +1,17 @@
 //middleware for server side validation
 
 const Joi = require("joi");
+//Joi.objectId for mongodb Object Id validation
+Joi.objectId = require("joi-objectid")(Joi);
 
 module.exports = {
   validateBody: schema => {
     return (req, res, next) => {
       const result = Joi.validate(req.body, schema);
       if (result.error) {
-        return res.status(400).json(result.error.details);
+        return res
+          .status(400)
+          .json(`${result.error.details[0].path} is required`);
       }
       if (!req.value) {
         req.value = {};
@@ -33,12 +37,12 @@ module.exports = {
     postSchema: Joi.object().keys({
       title: Joi.string().required(),
       article: Joi.string().required(),
-      userId: Joi.string().required(),
+      userId: Joi.objectId().required(),
       userName: Joi.string().required()
     }),
     commentSchema: Joi.object().keys({
-      postId: Joi.string().required(),
-      userId: Joi.string().required(),
+      postId: Joi.objectId().required(),
+      userId: Joi.objectId().required(),
       userName: Joi.string().required(),
       body: Joi.string().required()
     })
